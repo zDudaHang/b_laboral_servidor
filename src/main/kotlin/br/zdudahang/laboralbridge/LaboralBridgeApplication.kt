634e8965912fc136cb76266a30a97f5d902b1ec7
zDudaHang <maria.eduarda.melo.hang@gmail.com>
2023-03-27 21:54:04 -0300

    Base

---
+++ src/main/kotlin/br/zdudahang/laboralbridge/LaboralBridgeApplication.kt
@@ -0,0 +1,11 @@
+package br.zdudahang.laboralbridge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LaboralBridgeApplication
+
+fun main(args: Array<String>) {
+    runApplication<LaboralBridgeApplication>(*args)
+}
---
+++ src/test/kotlin/br/zdudahang/laboralbridge/LaboralBridgeApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.zdudahang.laboralbridge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LaboralBridgeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
